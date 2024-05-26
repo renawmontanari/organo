e2717ffb25b34e85753817e68d67d04c4f8d4dcf
renawmontanari <regiangelica@gmail.com>
2024-05-26 15:46:10 -0300

    componente-time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
+import Time from "./componentes/Time";
 
 function App() {
 
@@ -17,8 +18,15 @@ function App() {
       <Formulario 
         aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicioando(colaborador)} 
       />
+      <Time nome="Programação" />
+      <Time nome="Frontend" />
+      <Time nome="Data Science" />
+      <Time nome="Devops" />
+      <Time nome="UX e Design" />
+      <Time nome="Mobile" />
+      <Time nome="Inovação e Gestão" />
     </div>
   );
-}
+};
 
 export default App;
