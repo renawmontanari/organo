5c470c5ac9c5c0064943316193cff71b3122f1d8
renawmontanari <regiangelica@gmail.com>
2024-05-26 14:47:44 -0300

    cadastrando-colaboradores

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicioando = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div>
       <Banner />
-      <Formulario />
+      <Formulario 
+        aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicioando(colaborador)} 
+      />
     </div>
   );
 }
