89bee2084fd9ab98b68fb27c43b2a3b58385e0bd
renawmontanari <regiangelica@gmail.com>
2024-05-27 19:25:03 -0300

    rodape

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,35 @@
+import "./Rodape.css";
+
+const Rodape = () => {
+  return (
+    <footer className="footer">
+      <section>
+        <ul>
+          <li>
+            <a href="facebook.com" target="_blank">
+              <img src="/imagens/fb.png" alt="Icone Facebook" />
+            </a>
+          </li>
+          <li>
+            <a href="twitter.com" target="_blank">
+              <img src="/imagens/tw.png" alt="Icone Twitter" />
+            </a>
+          </li>
+          <li>
+            <a href="instagram.com" target="_blank">
+              <img src="/imagens/ig.png" alt="Icone Instagram" />
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <img src="/imagens/logo.png" alt="" />
+      </section>
+      <section>
+        <p><a href="https://prwm.vercel.app" target="_blank" rel="noreferrer">Desenvolvido por Renan W. Montanari</a></p>
+      </section>
+    </footer>
+  );
+};
+
+export default Rodape;
